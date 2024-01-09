aa14675246c0f624cd6cc397f43aa1bbedc217af
JoannaMoussa <joannamoussa.jm@gmail.com>
2024-01-09 17:15:54 +0100

    Add my skills section

--- resources/js/annotations.js
+++ resources/js/annotations.js
@@ -1,7 +1,18 @@
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  const html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const annotate = RoughNotation.annotate;
 
-  // collecting elements to be highlighted in pink
+  // pink highlight annotation
   const elementsToHighlightPink =
     document.getElementsByClassName("highlight-pink");
 
@@ -12,4 +23,37 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     pinkHighlight.show();
   }
+
+  // purple highlight annotation
+  const elementsToHighlightPurple =
+    document.getElementsByClassName("highlight-purple");
+
+  for (let i = 0; i < elementsToHighlightPurple.length; i++) {
+    const purpleHighlight = annotate(elementsToHighlightPurple[i], {
+      type: "highlight",
+      color: "var(--clr-accent-2)",
+    });
+    purpleHighlight.show();
+  }
+
+  // bracket annotation
+  const reactSkillsGrid = document.getElementById("annotate-bracket");
+
+  const bracketAnnotation = annotate(reactSkillsGrid, {
+    type: "bracket",
+    color: "var(--clr-text)",
+    brackets: "left",
+  });
+
+  if (isInViewport(reactSkillsGrid)) {
+    bracketAnnotation.hide();
+    bracketAnnotation.show();
+  }
+
+  document.addEventListener("scroll", () => {
+    if (!bracketAnnotation.isShowing() && isInViewport(reactSkillsGrid)) {
+      bracketAnnotation.hide();
+      bracketAnnotation.show();
+    }
+  });
 });
