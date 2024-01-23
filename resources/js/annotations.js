408be7547554cc4a43a34819ac0d5939de66bd91
JoannaMoussa <joannamoussa.jm@gmail.com>
2024-01-23 11:56:23 +0100

    Add scroll animations

--- resources/js/annotations.js
+++ resources/js/annotations.js
@@ -16,46 +16,67 @@ document.addEventListener("DOMContentLoaded", () => {
   const elementsToHighlightPink =
     document.getElementsByClassName("highlight-pink");
 
+  const pinkAnnotations = [];
   for (let i = 0; i < elementsToHighlightPink.length; i++) {
     const pinkHighlight = annotate(elementsToHighlightPink[i], {
       type: "highlight",
       color: "var(--clr-accent-1)",
       multiline: true,
     });
-    pinkHighlight.show();
+    pinkAnnotations.push(pinkHighlight);
   }
 
   // purple highlight annotation
   const elementsToHighlightPurple =
     document.getElementsByClassName("highlight-purple");
 
+  const purpleAnnotations = [];
   for (let i = 0; i < elementsToHighlightPurple.length; i++) {
     const purpleHighlight = annotate(elementsToHighlightPurple[i], {
       type: "highlight",
       color: "var(--clr-accent-2)",
       multiline: true,
     });
-    purpleHighlight.show();
+    purpleAnnotations.push(purpleHighlight);
   }
 
   // bracket annotation
   const reactSkillsGrid = document.getElementById("annotate-bracket");
-
   const bracketAnnotation = annotate(reactSkillsGrid, {
     type: "bracket",
     color: "var(--clr-text)",
     brackets: "left",
   });
 
-  if (isInViewport(reactSkillsGrid)) {
-    bracketAnnotation.hide();
-    bracketAnnotation.show();
+  const elementsToAnnotate = Array.from(elementsToHighlightPink).concat(
+    Array.from(elementsToHighlightPurple),
+    [reactSkillsGrid]
+  );
+  const annotations = pinkAnnotations.concat(purpleAnnotations, [
+    bracketAnnotation,
+  ]);
+  const isShowing = Array(elementsToAnnotate.length).fill(false);
+
+  // Initialization - same code on scroll event
+  for (let i = 0; i < elementsToAnnotate.length; i++) {
+    if (!isShowing[i] && isInViewport(elementsToAnnotate[i])) {
+      isShowing[i] = true;
+      setTimeout(() => {
+        annotations[i].hide();
+        annotations[i].show();
+      }, 1100);
+    }
   }
 
   document.addEventListener("scroll", () => {
-    if (!bracketAnnotation.isShowing() && isInViewport(reactSkillsGrid)) {
-      bracketAnnotation.hide();
-      bracketAnnotation.show();
+    for (let i = 0; i < elementsToAnnotate.length; i++) {
+      if (!isShowing[i] && isInViewport(elementsToAnnotate[i])) {
+        isShowing[i] = true;
+        setTimeout(() => {
+          annotations[i].hide();
+          annotations[i].show();
+        }, 1100);
+      }
     }
   });
 });
